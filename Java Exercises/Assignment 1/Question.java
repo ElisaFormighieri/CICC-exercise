9b0959b0b96f39fbd7eb2a57a7f2bc0562e524ca
elisa <elisaformighieri.als@gmail.com>
2022-10-31 19:11:41 -0300

    project done

--- Java Exercises/Assignment 1/Question.java
+++ Java Exercises/Assignment 1/Question.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Question {
-    public static void main(String args[]) {
-        System.out.println("Type the number: ");
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        program(a);
-    }
+  public static void main(String args[]) {
+    System.out.println("Type the number: ");
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    program(a);
+  }
 
-    public static void program(int a) {
-        System.out.println(a * 10);
-    }
+  public static void program(int a) {
+    System.out.println(a * 10);
+  }
 }
